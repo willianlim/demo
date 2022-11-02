4b71e3a9151b9a4283b6a7c563d6dc1a5fe97d00
willianlim <willianrosendodelima@outlook.com>
2022-11-02 19:48:31 -0300

    Proxy request to Backend

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -2,9 +2,15 @@ import {
     Button,
     Radio
 } from "antd";
+
+import { getAllStudents } from "./client";
+
 import './App.css';
 
 function App() {
+  getAllStudents()
+    .then(res => res.json())
+    .then(console.log)
   return (
     <div className="App">
       <Button type="primary">hello</Button>
