187df2d539e510c809aee807fece0beebf6d87dc
willianlim <willianrosendodelima@outlook.com>
2022-11-03 20:35:39 -0300

    Lets explore our frontend files

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,8 +1,3 @@
-import {
-    Button,
-    Radio
-} from "antd";
-
 import { getAllStudents } from "./client";
 
 import './App.css';
@@ -11,17 +6,7 @@ function App() {
   getAllStudents()
     .then(res => res.json())
     .then(console.log)
-  return (
-    <div className="App">
-      <Button type="primary">hello</Button>
-      <br/>
-      <Radio.Group value='large'>
-          <Radio.Button value="large">Large</Radio.Button>
-          <Radio.Button value="default">Default</Radio.Button>
-          <Radio.Button value="small">Small</Radio.Button>
-      </Radio.Group>
-    </div>
-  );
+  return ("Hello react");
 }
 
 export default App;
